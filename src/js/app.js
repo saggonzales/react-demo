33432e4250842fa7197178add746d394bcc43644
Vener Guevarra <vnrguevarra@gmail.com>
2018-03-04 22:51:43 +0800

    Added basic component rendering

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,17 @@
+
 import React, { Component } from 'react';
-import { reactDom } from 'react-dom';
+import { render } from 'react-dom';
+import createReactClass from 'create-react-class';
+
+
+// Create a component named MessageComponent
+var MessageComponent = createReactClass({
+  render: function() {
+    return (
+      <div>{this.props.message}</div>
+    );
+  }
+});
+
+// Render an instance of MessageComponent into document.body
+render( <MessageComponent message="Hello!" />, document.getElementById("app"));
